8c70ec68b8c2de82675b2edf8ec654e2c8568b95
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-06-03 13:19:21 +0100

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <footer class="app-bg primary-text footer">
+            Challenge by 
+            <a  className="secondary-text" href="https://www.frontendmentor.io?ref=challenge" rel="noopener" target="_blank"> Frontend Mentor</a>. 
+            Coded by <a className="secondary-text" rel="noopener"  href="https://www.frontendmentor.io/profile/marcoberdiano"> Marco</a>.
+        </footer>
+     );
+}
+ 
+export default Footer;
